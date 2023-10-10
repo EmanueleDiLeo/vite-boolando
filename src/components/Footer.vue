b33da83f2face42ef0d8ef2b2c087512b5bb41d2
EmanueleDiLeo <dileoemanuele11@gmail.com>
2023-10-10 17:35:03 +0200

    [ADD]: Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,25 @@
 <template>
   <footer>
-    Footer
+    <div class="container flex">
+      <div>
+        <h3>Booleando s.r.l</h3>
+        <span>Informazioni legali</span>
+        <span>Informativa sulla privacy</span>
+        <span>Diritto di recesso</span>
+      </div>
+
+      <div>
+        <span>Trovaci anche su</span>
+
+        <ul class="flex">
+          <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
+          <li><a href="#"><i class="fa-brands fa-square-facebook"></i></a></li>
+          <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+          <li><a href="#"><i class="fa-brands fa-pinterest"></i></a></li>
+          <li><a href="#"><i class="fa-brands fa-youtube"></i></a></li>
+        </ul>
+      </div>
+    </div>
   </footer>
 </template>
 
@@ -11,5 +30,29 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  @use '../scss/partials/variabiles' as *;
+  @use '../scss/partials/utiles' as *;
+  @use '../scss/partials/general' as *;
 
+  footer{
+  padding:30px 0px 20px 0px;
+  background-color: $bg-footer-color;
+  color: $link-color;
+  .container{
+    justify-content: space-between;
+    align-items: center;
+  }
+  span{
+    margin-right: 10px;
+  }
+  h3{
+    padding-bottom: 10px;
+  }
+  ul{
+    margin-top: 10px;
+    a{
+      margin-right: 10px;
+    }
+  }
+}
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,24 @@
 <template>
   <main>
-    Main
+    <div class="container flex">
+      <!-- <div class="card">
+        <div class="cont-img">
+          <img src="img/1.webp" alt="levi" title="levi">
+          <img class="switch" src="img/1b.webp" alt="levi" title="levi">
+        </div>
+
+        <span class="offer box-position">-50%</span>
+        <span class="green box-position">Sostenibilità</span>
+
+        <span class="heart">&hearts;</span>
+
+        <span>Levi's</span>
+        <h2>RELAXED FIT TEE UNISEX</h2>
+        <span class="price">14,99 &euro; </span>
+        <span class="old-price"> 29,99 &euro;</span>
+        
+      </div> -->
+    </div>
   </main>
 </template>
 
@@ -11,5 +29,40 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  @use '../scss/partials/variabiles' as *;
+  @use '../scss/partials/utiles' as *;
+  @use '../scss/partials/general' as *;
+
+  main{
+    margin: 150px 0px 30px 0px;
+    .container{
+      flex-wrap: wrap;
+      justify-content: center;
+      align-content: start;
+
+      .card{
+        position: relative;
+        width:calc(100% / 3.3);
+        margin-bottom: 60px;
+        min-width: 250px;
+        margin-right: 20px;
+        .cont-img{
+          cursor: pointer;
+
+          img{
+            left: 0px;
+            top: 0px;
+            max-width: 100%;
+            max-height:auto;
+          }
+        }
+      }
+
+    }
+  }
+
+
+
+
 
 </style>
